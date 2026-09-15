310b137a62ecbf5eba327910e9d1aa1fcd504069
IM404 <liuwenlong404@gmail.com>
2026-09-15 15:54:10 +0800

    fix: remove duplicate import of compressVideoPreset in presets index

--- lib/ffmpeg/presets/index.ts
+++ lib/ffmpeg/presets/index.ts
@@ -1,6 +1,6 @@
+import { compressVideoPreset } from "./compress-video";
 import { resizeVideoPreset } from "./resize-video";
 import { videoToMp3Preset } from "./video-to-mp3";
-import { compressVideoPreset } from "./compress-video";
 
 export const presets = [
   videoToMp3Preset,
