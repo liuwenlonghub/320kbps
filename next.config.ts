f198d68b7c009c58d08c0275ef9074175be94096
IM404 <liuwenlong404@gmail.com>
2026-09-16 10:31:03 +0800

    feat: add comment to clarify basePath in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/320kbps",
+  basePath: "/320kbps", // Set the base path for the application (If you are deploying to a subdirectory, set this to the subdirectory name)
 };
 
 export default nextConfig;
