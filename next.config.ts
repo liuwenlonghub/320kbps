12124806231e80b0d5715a82ffb5aa04fcf2b1cc
IM404 <liuwenlong404@gmail.com>
2026-09-16 10:28:52 +0800

    feat: remove comment from basePath in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/320kbps",  // Set the base path for the application
+  basePath: "/320kbps",
 };
 
 export default nextConfig;
